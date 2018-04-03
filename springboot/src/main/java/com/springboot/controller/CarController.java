bf80bc734d21fefbc94bd6b58a76dd33cb1e13ca
Kevi1n <786628143@qq.com>
2018-04-03 11:50:32 +0800

    CarController commit

---
+++ springboot/src/main/java/com/springboot/controller/CarController.java
@@ -0,0 +1,28 @@
+package com.springboot.controller;
+
+import com.springboot.pojo.Car;
+import com.springboot.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public ResponseEntity<?> list(){
+        List<Car> list = carService.list();
+
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+}
