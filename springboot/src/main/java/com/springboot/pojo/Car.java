ec8bdd2f7d1e44c27037f2ff13ca6deba5a65f1b
Kevi1n <786628143@qq.com>
2018-04-03 11:38:16 +0800

    car 实体类

---
+++ springboot/src/main/java/com/springboot/pojo/Car.java
@@ -0,0 +1,70 @@
+package com.springboot.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 汽车实体类
+ *
+ * @author JiangXH
+ */
+public class Car implements Serializable{
+    private static final long serialVersionUID = -5821651550212075426L;
+
+    private Integer id;
+    private String name;
+    private Double price;
+    private Date createDate;
+
+    public Car() {
+    }
+
+    public Car(Integer id, String name, Double price, Date createDate) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.createDate = createDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", createDate=" + createDate +
+                '}';
+    }
+}
