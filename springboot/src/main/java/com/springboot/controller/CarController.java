f9a37eee33564c2de19177e93d5f8b9e6eb55af2
Kevi1n <786628143@qq.com>
2018-04-08 10:06:40 +0800

    thymeleaf expression commit

--- springboot/src/main/java/com/springboot/controller/CarController.java
+++ springboot/src/main/java/com/springboot/controller/CarController.java
@@ -5,9 +5,7 @@ import com.springboot.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -20,6 +18,7 @@ import java.util.List;
  * @author JiangXh
  */
 @Controller
+@SessionAttributes(types = {String.class},names = {"car"})
 public class CarController {
 
     @Autowired
